7f112ac7044ef278cf1b936a5ee20c9777a17dba
Debashis Panigrahi <debashis.panigrahi@capgemini.com>
2025-05-31 14:11:57 +0530

    CSS issue

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from '../register/register.component';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,14 +8,8 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   registerMode = false;
-  users: any;
-  http = inject(HttpClient);
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
 
   registerToggle() {
     this.registerMode = true;
@@ -25,13 +18,5 @@ export class HomeComponent implements OnInit {
   cancelRegisterMode($event: boolean) {
     this.registerMode = $event;
     console.log('Register mode cancelled:', this.registerMode);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response: any) => (this.users = response),
-      error: (error: any) => console.log(error),
-      complete: () => console.log('Request has completed'),
-    });
   }
 }
